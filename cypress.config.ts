d5895fafd1064dab60bdf2b5f0b143793cbce7fc
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-05-14 18:36:21 -0600

    chore(workflows): add allure deps

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "cypress";
+const { allureCypress } = require("allure-cypress/reporter");
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
+    setupNodeEvents(on, config) {
+      allureCypress(on);
+    },
   },
 });
