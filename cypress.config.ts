4843c9b7119ef558b4714b8c609ada094374a27f
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-05-17 14:39:12 -0600

    chore(cypress): upload screenshots and videos for failed tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,26 @@
 import { defineConfig } from "cypress";
-const { allureCypress } = require("allure-cypress/reporter");
+const fs = require("fs");
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
+    video: true,
     setupNodeEvents(on, config) {
-      allureCypress(on);
+      on(
+        "after:spec",
+        (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+          if (results && results.video) {
+            // Do we have failures for any retry attempts?
+            const failures = results.tests.some((test) =>
+              test.attempts.some((attempt) => attempt.state === "failed"),
+            );
+            if (!failures) {
+              // delete the video if the spec passed and no tests retried
+              fs.unlinkSync(results.video);
+            }
+          }
+        },
+      );
     },
   },
 });
