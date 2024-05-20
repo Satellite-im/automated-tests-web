48e5236b4bd89ba6a95b64322aa19a9d744261d4
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-05-20 14:32:06 -0600

    test(update): fix location issue

--- cypress/e2e/01-pin-input.cy.ts
+++ cypress/e2e/01-pin-input.cy.ts
@@ -1,21 +1,21 @@
 describe("Pin Screen", () => {
   it("Enter Pin Screen - Enter valid PIN", () => {
     cy.visit("/");
+    cy.location("href").should("include", "/auth/unlock");
     cy.getByTestAttr("pin-keypad").should("exist");
-    cy.url().should("include", "/auth/unlock");
     cy.getByTestAttr("button-pin-1").should("exist").click();
     cy.getByTestAttr("button-pin-2").should("exist").click();
     cy.getByTestAttr("button-pin-3").should("exist").click();
     cy.getByTestAttr("button-pin-4").should("exist").click();
     cy.getByTestAttr("button-confirm-pin").should("exist").click();
     cy.getByTestAttr("button-confirm-pin").should("not.exist");
-    cy.url().should("include", "/pre");
-    cy.url().should("include", "/chat");
+    cy.location("href").should("include", "/pre");
+    cy.location("href").should("include", "/chat");
   });
 
   it("Enter Pin Screen - Enter PIN with 3 digits", () => {
     cy.visit("/");
-    cy.url().should("include", "/auth/unlock");
+    cy.location("href").should("include", "/auth/unlock");
     cy.getByTestAttr("button-pin-1").should("exist").click();
     cy.getByTestAttr("button-pin-2").should("exist").click();
     cy.getByTestAttr("button-pin-3").should("exist").click();
