265edf7eef8aadbe38d9cdff4ae6e80392e4cf03
Luis E <35935591+luisecm@users.noreply.github.com>
2024-06-21 10:38:21 -0600

    add(test): tests for settings notifications about license accesibility (#44)

--- cypress/e2e/14-settings-license.cy.ts
+++ cypress/e2e/14-settings-license.cy.ts
@@ -3,6 +3,7 @@ import authNewAccount from "./PageObjects/AuthNewAccount";
 import chatsMain from "./PageObjects/ChatsMain";
 import loginPinPage from "./PageObjects/LoginPin";
 import settingsProfile from "./PageObjects/Settings/SettingsProfile";
+import settingsLicenses from "./PageObjects/Settings/SettingsLicenses";
 
 describe("Settings - License", () => {
   const username = faker.internet.userName();
@@ -13,10 +14,20 @@ describe("Settings - License", () => {
     authNewAccount.createRandomUser(username, status);
     chatsMain.validateChatsMainPageIsShown();
     chatsMain.goToSettings();
-    settingsProfile.buttonAudioAndVideo.click();
+    settingsProfile.buttonLicenses.click();
   });
 
+  // Skipped since the licenses button does not perform any action
   it.skip("S1 - Clicking View License should take user to our licenses page", () => {
-    // Test code for S1
+    cy.url().should("include", "/settings/licenses");
+    // Label and texts for settings section are correct
+    settingsLicenses.licensesSectionLabel.should("have.text", "Uplink");
+    settingsLicenses.licensesSectionText.should(
+      "have.text",
+      "Both code and icons are under the MIT license.",
+    );
+
+    // Click on licenses button to show Uplink license
+    settingsLicenses.licensesSectionButton.click();
   });
 });
--- cypress/e2e/16-settings-accessibility.cy.ts
+++ cypress/e2e/16-settings-accessibility.cy.ts
@@ -3,6 +3,7 @@ import authNewAccount from "./PageObjects/AuthNewAccount";
 import chatsMain from "./PageObjects/ChatsMain";
 import loginPinPage from "./PageObjects/LoginPin";
 import settingsProfile from "./PageObjects/Settings/SettingsProfile";
+import SettingsAccessibility from "./PageObjects/Settings/SettingsAccessibility";
 
 describe("Settings - Accessibility", () => {
   const username = faker.internet.userName();
@@ -13,10 +14,67 @@ describe("Settings - Accessibility", () => {
     authNewAccount.createRandomUser(username, status);
     chatsMain.validateChatsMainPageIsShown();
     chatsMain.goToSettings();
-    settingsProfile.buttonAudioAndVideo.click();
+    settingsProfile.buttonAccessibility.click();
   });
 
-  it.skip("", () => {
-    // Test code for R1
+  it("P1, P2 - User should be able to toggle on/off Dyslexic mode and changes are applied everywhere", () => {
+    // Assert URL displayed
+    cy.url().should("include", "/settings/accessibility");
+
+    // Label and texts for settings section are correct
+    SettingsAccessibility.openDyslexicSectionLabel.should(
+      "have.text",
+      "Open Dyslexic",
+    );
+    SettingsAccessibility.openDyslexicSectionText.should(
+      "have.text",
+      "Open Dyslexic may help some users who suffer from dyslexia, it's a custom font you can enable.",
+    );
+
+    // Checkbox should be disabled by default
+    SettingsAccessibility.openDyslexicSectionCheckbox.should("not.be.checked");
+
+    // User can toggle checkbox to on
+    SettingsAccessibility.openDyslexicSectionSlider.click();
+    SettingsAccessibility.openDyslexicSectionCheckbox.should("be.checked");
+
+    // Validate font size applied everywhere through the app
+    SettingsAccessibility.openDyslexicSectionText.should(
+      "have.css",
+      "font-family",
+      "OpenDyslexic",
+    );
+
+    // Validate font size applied on different page - Main Chat
+    SettingsAccessibility.goToChat();
+    cy.contains("Let's get something started!").should(
+      "have.css",
+      "font-family",
+      "OpenDyslexic",
+    );
+
+    // Return to Settings Accessibility page
+    chatsMain.goToSettings();
+    settingsProfile.buttonAccessibility.click();
+    cy.url().should("include", "/settings/accessibility");
+
+    // User can toggle again checkbox to off
+    SettingsAccessibility.openDyslexicSectionSlider.click();
+    SettingsAccessibility.openDyslexicSectionCheckbox.should("not.be.checked");
+
+    // Validate font size was restored to default everywhere through the app
+    SettingsAccessibility.openDyslexicSectionText.should(
+      "have.css",
+      "font-family",
+      "Poppins",
+    );
+
+    // Validate font size was restored to default everywhere through the app - Main Chat
+    SettingsAccessibility.goToChat();
+    cy.contains("Let's get something started!").should(
+      "have.css",
+      "font-family",
+      "Poppins",
+    );
   });
 });
