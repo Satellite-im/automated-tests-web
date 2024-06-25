1d31e05ce7c556bc5bfc07c248d1cdde80b1074d
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2024-06-25 23:27:00 +0100

    cypress update

--- cypress/e2e/12-settings-notifications.cy.ts
+++ cypress/e2e/12-settings-notifications.cy.ts
@@ -15,89 +15,63 @@ describe("Settings - Accessibility", () => {
     chatsMain.validateChatsMainPageIsShown();
     chatsMain.goToSettings();
     settingsProfile.buttonNotifications.click();
+    cy.url().should("include", "/settings/notifications");
   });
 
+  function verifyToggleFunctionality(slider, checkbox) {
+    // Click the slider and wait for the UI to update
+    slider.click().then(() => {
+      // After the slider click, assert checkbox state
+      checkbox.should("not.be.checked");
+      // Click the slider again and wait for the UI to update
+      slider.click().then(() => {
+        // After the second slider click, assert checkbox state
+        checkbox.should("be.checked");
+      });
+    });
+  }
+  
   it("Q1 - User should be able to toggle on/off Notifications", () => {
-    cy.url().should("include", "/settings/notifications");
     // Label and texts for settings section are correct
     settingsNotifications.enabledSectionLabel.should("have.text", "Enabled");
     settingsNotifications.enabledSectionText.should(
       "have.text",
       "Enable notifications for incoming calls, messages, and more.",
     );
-
-    // Checkbox should be enabled by default
     settingsNotifications.enabledSectionCheckbox.should("be.checked");
-
-    // User can toggle checkbox to disable
-    settingsNotifications.enabledSectionSlider.click();
-    settingsNotifications.enabledSectionCheckbox.should("not.be.checked");
-
-    // User can toggle again checkbox to on
-    settingsNotifications.enabledSectionSlider.click();
-    settingsNotifications.enabledSectionCheckbox.should("be.checked");
+    verifyToggleFunctionality(settingsNotifications.enabledSectionSlider, settingsNotifications.enabledSectionCheckbox);
   });
 
   it("Q2 - User should be able to toggle on/off Friend Request Notifications", () => {
-    cy.url().should("include", "/settings/notifications");
     // Label and texts for settings section are correct
     settingsNotifications.friendsSectionLabel.should("have.text", "Friends");
     settingsNotifications.friendsSectionText.should(
       "have.text",
       "Enable notifications for friend requests.",
     );
-
-    // Checkbox should be enabled by default
     settingsNotifications.friendsSectionCheckbox.should("be.checked");
-
-    // User can toggle checkbox to disabled
-    settingsNotifications.friendsSectionSlider.click();
-    settingsNotifications.friendsSectionCheckbox.should("not.be.checked");
-
-    // User can toggle again checkbox to on
-    settingsNotifications.friendsSectionSlider.click();
-    settingsNotifications.friendsSectionCheckbox.should("be.checked");
+    verifyToggleFunctionality(settingsNotifications.friendsSectionSlider, settingsNotifications.friendsSectionCheckbox);
   });
 
   it("Q3 - User should be able to toggle on/off Message Notifications", () => {
-    cy.url().should("include", "/settings/notifications");
     // Label and texts for settings section are correct
     settingsNotifications.messagesSectionLabel.should("have.text", "Messages");
     settingsNotifications.messagesSectionText.should(
       "have.text",
       "Enable notifications for incoming messages.",
     );
-
-    // Checkbox should be enabled by default
     settingsNotifications.messagesSectionCheckbox.should("be.checked");
-
-    // User can toggle checkbox to off
-    settingsNotifications.messagesSectionSlider.click();
-    settingsNotifications.messagesSectionCheckbox.should("not.be.checked");
-
-    // User can toggle again checkbox to on
-    settingsNotifications.messagesSectionSlider.click();
-    settingsNotifications.messagesSectionCheckbox.should("be.checked");
+    verifyToggleFunctionality(settingsNotifications.messagesSectionSlider, settingsNotifications.messagesSectionCheckbox);
   });
 
-  it("Q4 -User should be able to toggle on/off Settings Notifications", () => {
-    cy.url().should("include", "/settings/notifications");
+  it("Q4 - User should be able to toggle on/off Settings Notifications", () => {
     // Label and texts for settings section are correct
     settingsNotifications.settingsSectionLabel.should("have.text", "Settings");
     settingsNotifications.settingsSectionText.should(
       "have.text",
       "Enable notifications for updates and important alerts.",
     );
-
-    // Checkbox should be enabled by default
     settingsNotifications.settingsSectionCheckbox.should("be.checked");
-
-    // User can toggle checkbox to off
-    settingsNotifications.settingsSectionSlider.click();
-    settingsNotifications.settingsSectionCheckbox.should("not.be.checked");
-
-    // User can toggle again checkbox to on
-    settingsNotifications.settingsSectionSlider.click();
-    settingsNotifications.settingsSectionCheckbox.should("be.checked");
+    verifyToggleFunctionality(settingsNotifications.settingsSectionSlider, settingsNotifications.settingsSectionCheckbox);
   });
 });
