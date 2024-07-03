46446e16e1a01c50075343b0f724891474bec604
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2024-07-03 15:45:34 +0100

    chore(cypress): minor suggestions (#62)

--- cypress/e2e/15-settings-developer.cy.ts
+++ cypress/e2e/15-settings-developer.cy.ts
@@ -6,10 +6,18 @@ import settingsProfile from "./PageObjects/Settings/SettingsProfile";
 import settingsAbout from "./PageObjects/Settings/SettingsAbout";
 import settingsDeveloper from "./PageObjects/Settings/SettingsDeveloper";
 
+const DEV_MODE_URL = "/settings/developer";
+const ABOUT_URL = "/settings/about";
+const BLUE_BORDER_COLOR = "rgb(77, 77, 255)";
+
 describe("Settings - Developer Mode", () => {
-  const username =
-    faker.person.firstName() + faker.number.int({ min: 100, max: 10000 });
+  const username = faker.person.firstName() + faker.number.int({ min: 100, max: 10000 });
   const status = faker.lorem.sentence(3);
+
+  const openDevMode = () => {
+    settingsAbout.openDevMode();
+    cy.url().should("include", DEV_MODE_URL);
+  };
 
   beforeEach(() => {
     loginPinPage.loginWithPin("1234");
@@ -21,33 +29,26 @@ describe("Settings - Developer Mode", () => {
 
   it("T1, T2 - Clicking Exit Devmode should exit user out of Devmode", () => {
     // Open DevMode
-    settingsAbout.openDevMode();
-    cy.url().should("include", "/settings/developer");
+    openDevMode();
+    cy.url().should("include", DEV_MODE_URL);
 
     // Validate header and description texts
     settingsDeveloper.devModeSectionLabel.should("have.text", "Devmode");
-    settingsDeveloper.devModeSectionText.should(
-      "have.text",
-      "Disable devmode.",
-    );
+    settingsDeveloper.devModeSectionText.should("have.text", "Disable devmode.");
 
     // Click on Exit DevMode
     settingsDeveloper.devModeSectionButton.click();
     settingsDeveloper.devModeSectionButton.should("not.exist");
-    cy.url().should("include", "/settings/about");
+    cy.url().should("include", ABOUT_URL);
   });
 
   it("T3 - Clicking Load MockData should load all mock data throughout app", () => {
     // Open DevMode
-    settingsAbout.openDevMode();
-    cy.url().should("include", "/settings/developer");
+    openDevMode();
 
     // Validate header and description texts
     settingsDeveloper.loadMockSectionLabel.should("have.text", "Load Mock");
-    settingsDeveloper.loadMockSectionText.should(
-      "have.text",
-      "Loads mock data into state.",
-    );
+    settingsDeveloper.loadMockSectionText.should("have.text", "Loads mock data into state.");
 
     // Click on Load Mock
     settingsDeveloper.loadMockSectionButton.click();
@@ -63,15 +64,12 @@ describe("Settings - Developer Mode", () => {
   // Skipped since button is not performing any action now
   it.skip("T4 - Clicking Clear State should clear users state", () => {
     // Open DevMode
-    settingsAbout.openDevMode();
-    cy.url().should("include", "/settings/developer");
+    openDevMode();
+    cy.url().should("include", DEV_MODE_URL);
 
     // Validate header and description texts
     settingsDeveloper.clearStateSectionLabel.should("have.text", "Clear State");
-    settingsDeveloper.clearStateSectionText.should(
-      "have.text",
-      "Reset the application state.",
-    );
+    settingsDeveloper.clearStateSectionText.should("have.text", "Reset the application state.");
 
     // Click on Clear State
     settingsDeveloper.clearStateSectionButton.click();
@@ -79,56 +77,43 @@ describe("Settings - Developer Mode", () => {
 
   it("T5 - Highlighted border should appear around Exit DevMode when clicked", () => {
     // Open DevMode
-    settingsAbout.openDevMode();
-    cy.url().should("include", "/settings/developer");
+    openDevMode();
+    cy.url().should("include", DEV_MODE_URL);
 
     // Focus on Exit DevMode
     settingsDeveloper.devModeSectionButton.focus();
 
     // Validate that the border color changes to blue
-    settingsDeveloper.devModeSectionButton.should(
-      "have.css",
-      "border-bottom-color",
-      "rgb(77, 77, 255)",
-    );
+    settingsDeveloper.devModeSectionButton.should("have.css", "border-bottom-color", BLUE_BORDER_COLOR);
   });
 
   it("T6 - Highlighted border should appear around Load MockData when clicked", () => {
     // Open DevMode
-    settingsAbout.openDevMode();
-    cy.url().should("include", "/settings/developer");
+    openDevMode();
+    cy.url().should("include", DEV_MODE_URL);
 
     // Click on Load Mock Data button
     settingsDeveloper.loadMockSectionButton.click();
 
     // Validate that the border color changes to blue
-    settingsDeveloper.loadMockSectionButton.should(
-      "have.css",
-      "border-bottom-color",
-      "rgb(77, 77, 255)",
-    );
+    settingsDeveloper.loadMockSectionButton.should("have.css", "border-bottom-color", BLUE_BORDER_COLOR);
   });
 
   it("T7 - Highlighted border should appear around Clear State when clicked", () => {
     // Open DevMode
-    settingsAbout.openDevMode();
-    cy.url().should("include", "/settings/developer");
+    openDevMode();
+    cy.url().should("include", DEV_MODE_URL);
 
     // Click on Clear State button
     settingsDeveloper.clearStateSectionButton.focus();
 
     // Validate that the border color changes to blue
-    settingsDeveloper.clearStateSectionButton.should(
-      "have.css",
-      "border-bottom-color",
-      "rgb(77, 77, 255)",
-    );
+    settingsDeveloper.clearStateSectionButton.should("have.css", "border-bottom-color", BLUE_BORDER_COLOR);
   });
 
   it("T8 - Clicking Test Voice State should open debug voice page", () => {
     // Open DevMode
-    settingsAbout.openDevMode();
-    cy.url().should("include", "/settings/developer");
+    openDevMode();
 
     // Validate header and description texts
     settingsDeveloper.testVoiceSectionLabel.should("have.text", "Test Voice");
