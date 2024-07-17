d8a77c95cf518f667b3884b1bca9bb43bc9cda93
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-07-17 11:17:36 -0600

    chore(playwright): add comment with test results for playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [["html"], ["json", { outputFile: "results.json" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   timeout: 120000,
   use: {
