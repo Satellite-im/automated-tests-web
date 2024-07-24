a48cafec18655e2da79a36bac89093fcc497619c
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-07-24 10:58:22 -0600

    add(test): add test for friend requests

--- playwright/specs/02-friends.spec.ts
+++ playwright/specs/02-friends.spec.ts
@@ -7,51 +7,73 @@ test.describe("Friends tests", () => {
   const status: string = faker.lorem.sentence(3);
   const statusTwo: string = faker.lorem.sentence(3);
 
-  test.beforeEach(async ({ createOrImportFirst, createOrImportSecond }) => {
-    // Start browser one
-    await createOrImportFirst.navigateTo();
+  test.beforeEach(
+    async ({
+      authNewAccountFirst,
+      authNewAccountSecond,
+      chatsMainPageFirst,
+      chatsMainPageSecond,
+      createOrImportFirst,
+      createOrImportSecond,
+      loginPinPageFirst,
+      loginPinPageSecond,
+      saveRecoverySeedFirst,
+      saveRecoverySeedSecond,
+    }) => {
+      // Start browser one
+      await createOrImportFirst.navigateTo();
 
-    // Start browser two
-    await createOrImportSecond.navigateTo();
-  });
+      // Start browser two
+      await createOrImportSecond.navigateTo();
 
-  test("Create two accounts and add them as friends", async ({
-    authNewAccountFirst,
-    authNewAccountSecond,
-    createOrImportFirst,
-    createOrImportSecond,
-    loginPinPageFirst,
-    loginPinPageSecond,
-    chatsMainPageFirst,
-    chatsMainPageSecond,
+      // Click on Create New Account
+      await createOrImportFirst.clickCreateNewAccount();
+
+      // Enter username and Status and click on create account
+      await authNewAccountFirst.validateLoadingHeader();
+      await authNewAccountFirst.typeOnUsername(username);
+      await authNewAccountFirst.typeOnStatus(status);
+      await authNewAccountFirst.clickOnCreateAccount();
+
+      // Enter Pin
+      await loginPinPageFirst.waitUntilPageIsLoaded();
+      await loginPinPageFirst.enterDefaultPin();
+
+      // Click on I Saved It
+      await saveRecoverySeedFirst.clickOnSavedIt();
+
+      // Go to Friends
+      await chatsMainPageFirst.goToFriends();
+
+      // Now with the second user, click on Create New Account
+      await createOrImportSecond.clickCreateNewAccount();
+
+      // Enter username, status and click on create account
+      await authNewAccountSecond.validateLoadingHeader();
+      await authNewAccountSecond.typeOnUsername(usernameTwo);
+      await authNewAccountSecond.typeOnStatus(statusTwo);
+      await authNewAccountSecond.clickOnCreateAccount();
+
+      // Enter a valid pin
+      await loginPinPageSecond.waitUntilPageIsLoaded();
+      await loginPinPageSecond.enterDefaultPin();
+
+      // Click on I Saved It
+      await saveRecoverySeedSecond.clickOnSavedIt();
+
+      // Go to Friends
+      await chatsMainPageSecond.goToFriends();
+    },
+  );
+
+  test("Send a friend request to remote user - Remote user accepts friend request", async ({
     friendsScreenFirst,
     friendsScreenSecond,
-    saveRecoverySeedFirst,
-    saveRecoverySeedSecond,
     context1,
     context2,
     page1,
     page2,
   }) => {
-    // Click on Create New Account
-    await createOrImportFirst.clickCreateNewAccount();
-
-    // Enter username and Status and click on create account
-    await authNewAccountFirst.validateLoadingHeader();
-    await authNewAccountFirst.typeOnUsername(username);
-    await authNewAccountFirst.typeOnStatus(status);
-    await authNewAccountFirst.clickOnCreateAccount();
-
-    // Enter Pin
-    await loginPinPageFirst.waitUntilPageIsLoaded();
-    await loginPinPageFirst.enterDefaultPin();
-
-    // Click on I Saved It
-    await saveRecoverySeedFirst.clickOnSavedIt();
-
-    // Go to Friends
-    await chatsMainPageFirst.goToFriends();
-
     // Grant clipboard permissions, Copy DID and save it into a constant
     await context1.grantPermissions(["clipboard-read", "clipboard-write"]);
     await friendsScreenFirst.copyDID();
@@ -59,25 +81,6 @@ test.describe("Friends tests", () => {
       navigator.clipboard.readText(),
     );
     const didKeyFirstUser = await handle.jsonValue();
-
-    // Now with the second user, click on Create New Account
-    await createOrImportSecond.clickCreateNewAccount();
-
-    // Enter username, status and click on create account
-    await authNewAccountSecond.validateLoadingHeader();
-    await authNewAccountSecond.typeOnUsername(usernameTwo);
-    await authNewAccountSecond.typeOnStatus(statusTwo);
-    await authNewAccountSecond.clickOnCreateAccount();
-
-    // Enter a valid pin
-    await loginPinPageSecond.waitUntilPageIsLoaded();
-    await loginPinPageSecond.enterDefaultPin();
-
-    // Click on I Saved It
-    await saveRecoverySeedSecond.clickOnSavedIt();
-
-    // Go to Friends
-    await chatsMainPageSecond.goToFriends();
 
     // Grant clipboard permissions, Copy DID and save it into a constant
     await context2.grantPermissions(["clipboard-read", "clipboard-write"]);
@@ -107,6 +110,42 @@ test.describe("Friends tests", () => {
     await friendsScreenSecond.goToRequestList();
     await friendsScreenSecond.goToAllFriendsList();
     await friendsScreenSecond.chatWithFriend(username);
+  });
+
+  test("Send a friend request to remote user - Remote user denies friend request", async ({
+    friendsScreenFirst,
+    friendsScreenSecond,
+    context1,
+    context2,
+    page1,
+    page2,
+  }) => {
+    // Grant clipboard permissions, Copy DID and save it into a constant
+    await context1.grantPermissions(["clipboard-read", "clipboard-write"]);
+    await friendsScreenFirst.copyDID();
+    const handle = await page1.evaluateHandle(() =>
+      navigator.clipboard.readText(),
+    );
+    const didKeyFirstUser = await handle.jsonValue();
+
+    // Grant clipboard permissions, Copy DID and save it into a constant
+    await context2.grantPermissions(["clipboard-read", "clipboard-write"]);
+    await friendsScreenSecond.copyDID();
+    const handleTwo = await page2.evaluateHandle(() =>
+      navigator.clipboard.readText(),
+    );
+
+    // Now, add the first user as a friend
+    await friendsScreenSecond.addFriend(didKeyFirstUser);
+    await friendsScreenSecond.goToBlockedList();
+    await friendsScreenSecond.goToRequestList();
+
+    // With First User, go to requests list and accept friend request
+    await friendsScreenFirst.goToRequestList();
+    await friendsScreenFirst.goToAllFriendsList();
+    await friendsScreenFirst.goToRequestList();
+    await friendsScreenFirst.validateIncomingRequestExists();
+    await friendsScreenFirst.denyFriendRequest(usernameTwo);
   });
 
   test.afterAll(async ({ page1, page2 }) => {
