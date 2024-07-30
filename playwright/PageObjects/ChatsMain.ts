2abe0fedd2c7a7c682ddb9dae600019dc7b9f272
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-07-30 16:14:54 -0600

    update(locator): more ui locators

--- playwright/PageObjects/ChatsMain.ts
+++ playwright/PageObjects/ChatsMain.ts
@@ -12,6 +12,13 @@ export class ChatsMainPage extends MainPage {
   readonly buttonChatbarStickerPicker: Locator;
   readonly buttonCreateGroupChat: Locator;
   readonly buttonMarketplace: Locator;
+  readonly chatEncryptedMessage: Locator;
+  readonly chatEncryptedMessageText: Locator;
+  readonly chatPreview: Locator;
+  readonly chatPreviewLastMessage: Locator;
+  readonly chatPreviewName: Locator;
+  readonly chatPreviewPicture: Locator;
+  readonly chatPreviewTimestamp: Locator;
   readonly chatbar: Locator;
   readonly chatbarInput: Locator;
   readonly createGroupButton: Locator;
@@ -21,6 +28,22 @@ export class ChatsMainPage extends MainPage {
   readonly createGroupLabelGroupName: Locator;
   readonly createGroupLabelSelectMembers: Locator;
   readonly inputAddAttachment: Locator;
+  readonly labelPinnedMessages: Locator;
+  readonly messageGroupLocal: Locator;
+  readonly messageGroupLocalProfilePicture: Locator;
+  readonly messageGroupRemote: Locator;
+  readonly messageGroupRemoteProfilePicture: Locator;
+  readonly messageGroupTimestamp: Locator;
+  readonly messageGroupUsername: Locator;
+  readonly pendingMessageGroup: Locator;
+  readonly pinnedMessage: Locator;
+  readonly pinnedMessageButtonGoTo: Locator;
+  readonly pinnedMessageButtonUnpin: Locator;
+  readonly pinnedMessageProfilePicture: Locator;
+  readonly pinnedMessageSender: Locator;
+  readonly pinnedMessageTimestamp: Locator;
+  readonly pinnedMessagesContainer: Locator;
+  readonly pinnedMessagesEmpty: Locator;
   readonly sectionAddSomeone: Locator;
   readonly topbar: Locator;
 
@@ -43,6 +66,13 @@ export class ChatsMainPage extends MainPage {
     );
 
     this.buttonMarketplace = page.getByTestId("button-marketplace");
+    this.chatEncryptedMessage = page.getByTestId("chat-encrypted-message");
+    this.chatEncryptedMessageText = page.getByTestId("chat-encrypted-text");
+    this.chatPreview = page.getByTestId("chat-preview");
+    this.chatPreviewLastMessage = page.getByTestId("chat-preview-last-message");
+    this.chatPreviewName = page.getByTestId("chat-preview-name");
+    this.chatPreviewPicture = page.getByTestId("chat-preview-picture");
+    this.chatPreviewTimestamp = page.getByTestId("chat-preview-timestamp");
     this.chatbar = page.getByTestId("chatbar");
     this.chatbarInput = page.getByTestId("chatbar-input");
     this.createGroupButton = page.getByTestId("button-create-group");
@@ -63,6 +93,34 @@ export class ChatsMainPage extends MainPage {
       .locator('[data-cy="button-add-attachment"]')
       .locator("xpath=..")
       .locator("input");
+    this.labelPinnedMessages = page.getByTestId("label-pinned-messages");
+    this.messageGroupLocal = page.getByTestId("message-group-local");
+    this.messageGroupLocalProfilePicture = page.getByTestId(
+      "message-group-local-profile-picture",
+    );
+    this.messageGroupRemote = page.getByTestId("message-group-remote");
+    this.messageGroupRemoteProfilePicture = page.getByTestId(
+      "message-group-remote-profile-picture",
+    );
+    this.messageGroupTimestamp = page.getByTestId("message-group-timestamp");
+    this.messageGroupUsername = page.getByTestId("message-group-username");
+    this.pendingMessageGroup = page.getByTestId("pending-message-group");
+    this.pinnedMessage = page.getByTestId("pinned-message");
+    this.pinnedMessageButtonGoTo = page.getByTestId(
+      "pinned-message-button-go-to",
+    );
+    this.pinnedMessageButtonUnpin = page.getByTestId(
+      "pinned-message-button-unpin",
+    );
+    this.pinnedMessageProfilePicture = page.getByTestId(
+      "pinned-message-profile-picture",
+    );
+    this.pinnedMessageSender = page.getByTestId("pinned-message-sender");
+    this.pinnedMessageTimestamp = page.getByTestId("pinned-message-timestamp");
+    this.pinnedMessagesContainer = page.getByTestId(
+      "pinned-messages-container",
+    );
+    this.pinnedMessagesEmpty = page.getByTestId("pinned-messages-empty");
     this.sectionAddSomeone = page.getByTestId("section-add-someone");
     this.topbar = page.getByTestId("topbar");
   }
