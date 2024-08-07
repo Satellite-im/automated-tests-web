82bdc0e20c1b9406e3a291900a99285d478bdc05
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-08-07 16:49:50 -0600

    add(test): add tests for account creation - more

--- playwright/PageObjects/AuthNewAccount.ts
+++ playwright/PageObjects/AuthNewAccount.ts
@@ -3,38 +3,47 @@ import { expect, type Locator, type Page } from "@playwright/test";
 
 export class AuthNewAccount extends MainPage {
   readonly page: Page;
-  readonly buttonNewAccountGoBack: Locator;
   readonly buttonNewAccountCreate: Locator;
-  readonly titleNewAccount: Locator;
-  readonly textNewAccountSecondary: Locator;
-  readonly profilePictureNewAccount: Locator;
-  readonly labelNewAccountUsername: Locator;
+  readonly buttonNewAccountFileUpload: Locator;
+  readonly buttonNewAccountGoBack: Locator;
+  readonly identiconNewAccount: Locator;
+  readonly inputNewAccountFileUpload: Locator;
+  readonly inputNewAccountStatus: Locator;
   readonly inputNewAccountUsername: Locator;
   readonly labelNewAccountStatus: Locator;
-  readonly inputNewAccountStatus: Locator;
+  readonly labelNewAccountUsername: Locator;
+  readonly profileImageNewAccount: Locator;
+  readonly profilePictureNewAccount: Locator;
+  readonly textNewAccountSecondary: Locator;
+  readonly titleNewAccount: Locator;
 
   constructor(page: Page) {
     super(page);
     this.page = page;
+
+    this.buttonNewAccountCreate = page.getByTestId("button-new-account-create");
+    this.buttonNewAccountFileUpload = page.getByTestId("button-file-upload");
     this.buttonNewAccountGoBack = page.getByTestId(
       "button-new-account-go-back",
     );
-    this.buttonNewAccountCreate = page.getByTestId("button-new-account-create");
-    this.titleNewAccount = page.getByTestId("title-new-account");
-    this.textNewAccountSecondary = page.getByTestId(
-      "text-new-account-secondary",
-    );
-    this.profilePictureNewAccount = page.getByTestId(
-      "profile-picture-new-account",
+    this.identiconNewAccount = page.locator(".identicon").locator("img");
+    this.inputNewAccountFileUpload = page.locator("input[type=file]");
+    this.inputNewAccountStatus = page.getByTestId("input-new-account-status");
+    this.inputNewAccountUsername = page.getByTestId(
+      "input-new-account-username",
     );
     this.labelNewAccountUsername = page.getByTestId(
       "label-new-account-username",
     );
-    this.inputNewAccountUsername = page.getByTestId(
-      "input-new-account-username",
-    );
     this.labelNewAccountStatus = page.getByTestId("label-new-account-status");
-    this.inputNewAccountStatus = page.getByTestId("input-new-account-status");
+    this.profileImageNewAccount = page.getByTestId("profile-image");
+    this.profilePictureNewAccount = page.getByTestId(
+      "profile-picture-new-account",
+    );
+    this.titleNewAccount = page.getByTestId("title-new-account");
+    this.textNewAccountSecondary = page.getByTestId(
+      "text-new-account-secondary",
+    );
   }
 
   async clickOnCreateAccount() {
@@ -46,6 +55,11 @@ export class AuthNewAccount extends MainPage {
     this.typeOnUsername(username);
     this.typeOnStatus(status);
     this.buttonNewAccountCreate.click();
+  }
+
+  async uploadProfilePicture(file: string) {
+    await this.buttonNewAccountFileUpload.click();
+    await this.inputNewAccountFileUpload.setInputFiles(file);
   }
 
   async typeOnStatus(status: string) {
