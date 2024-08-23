3b38efafad713cd9a7da07914b4cd7e2d57e2234
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-08-23 16:26:04 -0600

    add(pom): new page objects for calls and quick profile

---
+++ playwright/PageObjects/CallSettings.ts
@@ -0,0 +1,134 @@
+import MainPage from "./MainPage";
+import { type Locator, type Page } from "@playwright/test";
+
+export class CallSettings extends MainPage {
+  readonly callSettings: Locator;
+  readonly audioBitrate: Locator;
+  readonly audioBitrateLabel: Locator;
+  readonly audioBitrateSelector: Locator;
+  readonly audioBitrateSelectorOption: Locator;
+  readonly audioBitrateText: Locator;
+  readonly audioChannels: Locator;
+  readonly audioChannelsLabel: Locator;
+  readonly audioChannelsSelector: Locator;
+  readonly audioChannelsSelectorOption: Locator;
+  readonly audioChannelsText: Locator;
+  readonly echoCancellation: Locator;
+  readonly echoCancellationCheckbox: Locator;
+  readonly echoCancellationSlider: Locator;
+  readonly echoCancellationLabel: Locator;
+  readonly echoCancellationText: Locator;
+  readonly gainControl: Locator;
+  readonly gainControlCheckbox: Locator;
+  readonly gainControlSlider: Locator;
+  readonly gainControlLabel: Locator;
+  readonly gainControlText: Locator;
+  readonly noiseSupression: Locator;
+  readonly noiseSupressionCheckbox: Locator;
+  readonly noiseSupressionSlider: Locator;
+  readonly noiseSupressionLabel: Locator;
+  readonly noiseSupressionText: Locator;
+  readonly sampleSize: Locator;
+  readonly sampleSizeLabel: Locator;
+  readonly sampleSizeSelector: Locator;
+  readonly sampleSizeSelectorOption: Locator;
+  readonly sampleSizeText: Locator;
+  readonly videoDevice: Locator;
+  readonly videoDeviceLabel: Locator;
+  readonly videoDeviceSelector: Locator;
+  readonly videoDeviceSelectorOption: Locator;
+  readonly videoDeviceText: Locator;
+
+  constructor(public readonly page: Page) {
+    super(page);
+    this.callSettings = this.page.locator(".call-settings");
+    this.audioBitrate = this.page.getByTestId("section-audio-bitrate");
+    this.audioBitrateLabel = this.audioBitrate.getByTestId(
+      "setting-section-label",
+    );
+    this.audioBitrateSelector = this.audioBitrate.getByTestId(
+      "selector-video-device",
+    );
+    this.audioBitrateSelectorOption =
+      this.audioBitrateSelector.getByTestId("select-option");
+    this.audioBitrateText = this.audioBitrate.getByTestId(
+      "setting-section-text",
+    );
+    this.audioChannels = this.page.getByTestId("section-audio-channels");
+    this.audioChannelsLabel = this.audioChannels.getByTestId(
+      "setting-section-label",
+    );
+    this.audioChannelsSelector = this.audioChannels.getByTestId(
+      "selector-video-device",
+    );
+    this.audioChannelsSelectorOption =
+      this.audioChannelsSelector.getByTestId("select-option");
+    this.audioChannelsText = this.audioChannels.getByTestId(
+      "setting-section-text",
+    );
+    this.sampleSizeLabel = this.sampleSize.getByTestId("setting-section-label");
+    this.sampleSizeSelector = this.sampleSize.getByTestId(
+      "selector-video-device",
+    );
+    this.sampleSizeSelectorOption =
+      this.sampleSizeSelector.getByTestId("select-option");
+    this.sampleSizeText = this.sampleSize.getByTestId("setting-section-text");
+
+    this.echoCancellation = this.page.getByTestId("section-echo-cancellation");
+    this.echoCancellationCheckbox = this.echoCancellation.getByTestId(
+      "switch-echo-cancellation",
+    );
+    this.echoCancellationSlider = this.echoCancellation.locator(
+      '[data-cy="section-echo-cancellation"] > .body > .content > .switch > .slider',
+    );
+    this.echoCancellationLabel = this.echoCancellation.getByTestId(
+      "setting-section-label",
+    );
+    this.echoCancellationText = this.echoCancellation.getByTestId(
+      "setting-section-text",
+    );
+    this.gainControl = this.page.getByTestId("section-gain-control");
+    this.gainControlCheckbox = this.gainControl.getByTestId(
+      "switch-echo-cancellation",
+    );
+    this.gainControlSlider = this.gainControl.locator(
+      '[data-cy="section-echo-cancellation"] > .body > .content > .switch > .slider',
+    );
+    this.gainControlLabel = this.gainControl.getByTestId(
+      "setting-section-label",
+    );
+    this.gainControlText = this.gainControl.getByTestId("setting-section-text");
+
+    this.noiseSupression = this.page.getByTestId("section-noise-supression");
+    this.noiseSupressionCheckbox = this.noiseSupression.getByTestId(
+      "switch-echo-cancellation",
+    );
+    this.noiseSupressionSlider = this.noiseSupression.locator(
+      '[data-cy="section-echo-cancellation"] > .body > .content > .switch > .slider',
+    );
+    this.noiseSupressionLabel = this.noiseSupression.getByTestId(
+      "setting-section-label",
+    );
+    this.noiseSupressionText = this.noiseSupression.getByTestId(
+      "setting-section-text",
+    );
+    this.sampleSize = this.page.getByTestId("section-sample-size");
+    this.sampleSizeLabel = this.sampleSize.getByTestId("setting-section-label");
+    this.sampleSizeSelector = this.sampleSize.getByTestId(
+      "selector-video-device",
+    );
+    this.sampleSizeSelectorOption =
+      this.sampleSizeSelector.getByTestId("select-option");
+    this.sampleSizeText = this.sampleSize.getByTestId("setting-section-text");
+    this.videoDevice = this.page.getByTestId("section-video-device");
+    this.videoDeviceLabel = this.videoDevice.getByTestId(
+      "setting-section-label",
+    );
+    this.videoDeviceSelector = this.videoDevice.getByTestId(
+      "selector-video-device",
+    );
+    this.videoDeviceSelectorOption =
+      this.videoDeviceSelector.getByTestId("select-option");
+    this.videoDeviceText = this.videoDevice.getByTestId("setting-section-text");
+  }
+}
---
+++ playwright/PageObjects/QuickProfile.ts
@@ -0,0 +1,246 @@
+import MainPage from "./MainPage";
+import { expect, type Locator, type Page } from "@playwright/test";
+
+export class QuickProfile extends MainPage {
+  readonly addSomeone: Locator;
+  readonly buttonAddAttachment: Locator;
+  readonly buttonAddFriends: Locator;
+  readonly buttonChatAddAttachment: Locator;
+  readonly buttonChatCall: Locator;
+  readonly buttonChatFavorite: Locator;
+  readonly buttonChatPin: Locator;
+  readonly buttonChatTransact: Locator;
+  readonly buttonChatVideo: Locator;
+  readonly buttonChatbarEmojiPicker: Locator;
+  readonly buttonChatbarGifPicker: Locator;
+  readonly buttonChatbarSendMessage: Locator;
+  readonly buttonChatbarStickerPicker: Locator;
+  readonly buttonCreateGroupChat: Locator;
+  readonly buttonMarketplace: Locator;
+  readonly chatEncryptedMessage: Locator;
+  readonly chatEncryptedMessageText: Locator;
+  readonly chatbar: Locator;
+  readonly chatbarInput: Locator;
+  readonly chatbarInputContainer: Locator;
+  readonly chatTopbarProfilePicture: Locator;
+  readonly chatTopbarProfilePictureImage: Locator;
+  readonly chatTopbarProfileStatusIndicator: Locator;
+  readonly chatTopbarStatus: Locator;
+  readonly chatTopbarUsername: Locator;
+  readonly coinAmountIndicator: Locator;
+  readonly contextMenuChatMessage: Locator;
+  readonly contextMenuOptionCopyMessage: Locator;
+  readonly contextMenuOptionFavorite: Locator;
+  readonly contextMenuOptionHide: Locator;
+  readonly contextMenuOptionMarkRead: Locator;
+  readonly contextMenuOptionPinMessage: Locator;
+  readonly contextMenuOptionReplyMessage: Locator;
+  readonly contextMenuOptionUnpinMessage: Locator;
+  readonly contextMenuSidebarChat: Locator;
+  readonly createGroupButton: Locator;
+  readonly createGroupModal: Locator;
+  readonly createGroupInputGroupName: Locator;
+  readonly createGroupLabelGroupMembers: Locator;
+  readonly createGroupLabelGroupName: Locator;
+  readonly createGroupLabelSelectMembers: Locator;
+  readonly emojiButton: Locator;
+  readonly emojiGroup: Locator;
+  readonly emojiPickerButton: Locator;
+  readonly inputAddAttachment: Locator;
+  readonly labelPinnedMessages: Locator;
+  readonly messageBubbleContent: Locator;
+  readonly messabeBubbleLocal: Locator;
+  readonly messageBubbleRemote: Locator;
+  readonly messageGroupLocal: Locator;
+  readonly messageGroupLocalProfilePicture: Locator;
+  readonly messageGroupLocalProfileStatusIndicator: Locator;
+  readonly messageGroupRemote: Locator;
+  readonly messageGroupRemoteProfilePicture: Locator;
+  readonly messageGroupRemoteProfileStatusIndicator: Locator;
+  readonly messageGroupTimestamp: Locator;
+  readonly messageGroupUsername: Locator;
+  readonly messagePinIndicator: Locator;
+  readonly pendingMessageGroup: Locator;
+  readonly pendingFileCancelButton: Locator;
+  readonly pendingFileName: Locator;
+  readonly pendingFileSize: Locator;
+  readonly pendingFileUploadProgress: Locator;
+  readonly pendingMessage: Locator;
+  readonly pendingMessageText: Locator;
+  readonly pinnedMessage: Locator;
+  readonly pinnedMessageButtonGoTo: Locator;
+  readonly pinnedMessageButtonUnpin: Locator;
+  readonly pinnedMessageProfilePicture: Locator;
+  readonly pinnedMessageProfileStatusIndicator: Locator;
+  readonly pinnedMessageSender: Locator;
+  readonly pinnedMessageText: Locator;
+  readonly pinnedMessageTimestamp: Locator;
+  readonly pinnedMessagesContainer: Locator;
+  readonly pinnedMessagesEmpty: Locator;
+  readonly sectionAddSomeone: Locator;
+  readonly statusIndicator: Locator;
+  readonly topbar: Locator;
+
+  constructor(public readonly page: Page) {
+    super(page);
+    this.addSomeone = this.page.locator(".add-someone");
+    this.buttonAddAttachment = this.page.getByTestId("button-add-attachment");
+    this.buttonAddFriends = this.page.getByTestId("button-add-friends");
+    this.buttonChatAddAttachment = this.page.getByTestId(
+      "button-chat-add-attachment",
+    );
+    this.buttonChatCall = this.page.getByTestId("button-chat-call");
+    this.buttonChatFavorite = this.page.getByTestId("button-chat-favorite");
+    this.buttonChatPin = this.page.getByTestId("button-chat-pin");
+    this.buttonChatTransact = this.page.getByTestId("button-chat-transact");
+    this.buttonChatVideo = this.page.getByTestId("button-chat-video");
+    this.buttonChatbarEmojiPicker = this.page.getByTestId(
+      "button-chatbar-emoji-picker",
+    );
+    this.buttonChatbarGifPicker = this.page.getByTestId(
+      "button-chatbar-gif-picker",
+    );
+    this.buttonChatbarSendMessage = this.page.getByTestId(
+      "button-chatbar-send-message",
+    );
+    this.buttonChatbarStickerPicker = this.page.getByTestId(
+      "button-chatbar-sticker-picker",
+    );
+    this.buttonCreateGroupChat = this.page.getByTestId(
+      "button-create-group-chat",
+    );
+    this.buttonMarketplace = this.page.getByTestId("button-marketplace");
+    this.chatEncryptedMessage = this.page.getByTestId("chat-encrypted-notice");
+    this.chatEncryptedMessageText = this.page.getByTestId(
+      "chat-encrypted-text",
+    );
+    this.chatbar = this.page.getByTestId("chatbar");
+    this.chatbarInput = this.page
+      .locator(".cm-editor")
+      .locator(".cm-scroller")
+      .getByRole("textbox");
+    this.chatbarInputContainer = this.page
+      .locator('[data-cy="chatbar-input"]')
+      .locator("xpath=..");
+    this.chatTopbarProfilePicture = this.page.getByTestId(
+      "chat-topbar-profile-picture",
+    );
+    this.chatTopbarProfilePictureImage =
+      this.chatTopbarProfilePicture.locator("img");
+    this.chatTopbarProfileStatusIndicator =
+      this.chatTopbarProfilePicture.getByTestId("status-indicator");
+    this.chatTopbarStatus = this.page.getByTestId("chat-topbar-status");
+    this.chatTopbarUsername = this.page.getByTestId("chat-topbar-username");
+    this.coinAmountIndicator = this.page
+      .getByTestId("topbar")
+      .locator("button")
+      .first()
+      .locator("p");
+    this.contextMenuChatMessage = this.page.getByTestId(
+      "context-menu-chat-message",
+    );
+    this.contextMenuOptionCopyMessage = this.page.getByTestId(
+      "context-menu-option-Copy",
+    );
+    this.contextMenuOptionFavorite = this.page.getByTestId(
+      "context-menu-option-Favorite",
+    );
+    this.contextMenuOptionHide = this.page.getByTestId(
+      "context-menu-option-Hide",
+    );
+    this.contextMenuOptionMarkRead = this.page.getByTestId(
+      "context-menu-option-Mark Read",
+    );
+    this.contextMenuOptionPinMessage = this.page.getByTestId(
+      "context-menu-option-Pin Message",
+    );
+    this.contextMenuOptionReplyMessage = this.page.getByTestId(
+      "context-menu-option-Reply",
+    );
+    this.contextMenuSidebarChat = this.page.getByTestId(
+      "context-menu-sidebar-chat",
+    );
+    this.contextMenuOptionUnpinMessage = this.page.getByTestId(
+      "context-menu-option-Unpin Message",
+    );
+    this.createGroupButton = this.page.getByTestId("button-create-group");
+    this.createGroupModal = this.page.getByTestId("modal-create-group-chat");
+    this.createGroupInputGroupName = this.page.getByTestId(
+      "input-create-group-name",
+    );
+    this.createGroupLabelGroupMembers = this.page.getByTestId(
+      "label-create-group-members",
+    );
+    this.createGroupLabelGroupName = this.page.getByTestId(
+      "label-create-group-name",
+    );
+    this.createGroupLabelSelectMembers = this.page.getByTestId(
+      "label-create-group-select-members",
+    );
+    this.emojiButton = this.page.locator('[data-cy^="button-emoji-"]');
+    this.emojiGroup = this.page.getByTestId("emoji-group");
+    this.emojiPickerButton = this.page.getByTestId("button-emoji-picker");
+    this.inputAddAttachment = this.page
+      .locator('[data-cy="button-add-attachment"]')
+      .locator("xpath=..")
+      .locator("input");
+    this.labelPinnedMessages = this.page.getByTestId("label-pinned-messages");
+    this.messageBubbleContent = this.page
+      .getByTestId("message-bubble-content")
+      .locator("p")
+      .locator("p");
+    this.messabeBubbleLocal = this.page.getByTestId("message-bubble-local");
+    this.messageBubbleRemote = this.page.getByTestId("message-bubble-remote");
+    this.messageGroupLocal = this.page.getByTestId("message-group-local");
+    this.messageGroupLocalProfilePicture = this.page.getByTestId(
+      "message-group-local-profile-picture",
+    );
+    this.messageGroupLocalProfileStatusIndicator =
+      this.messageGroupLocalProfilePicture.getByTestId("status-indicator");
+    this.messageGroupRemote = this.page.getByTestId("message-group-remote");
+    this.messageGroupRemoteProfilePicture = this.page.getByTestId(
+      "message-group-remote-profile-picture",
+    );
+    this.messageGroupRemoteProfileStatusIndicator =
+      this.messageGroupRemoteProfilePicture.getByTestId("status-indicator");
+    this.messageGroupTimestamp = this.page.getByTestId(
+      "message-group-timestamp",
+    );
+    this.messageGroupUsername = this.page.getByTestId("message-group-username");
+    this.messagePinIndicator = this.page.getByTestId("message-pin-indicator");
+    this.pendingFileCancelButton = this.page.getByTestId(
+      "button-pending-file-cancel",
+    );
+    this.pendingFileName = this.page.getByTestId("pending-file-name");
+    this.pendingFileSize = this.page.getByTestId("pending-file-size");
+    this.pendingFileUploadProgress = this.page.getByTestId(
+      "pending-file-upload-progress",
+    );
+    this.pendingMessage = this.page.getByTestId("pending-message");
+    this.pendingMessageText = this.page.getByTestId("pending-message-text");
+    this.pendingMessageGroup = this.page.getByTestId("pending-message-group");
+    this.pinnedMessage = this.page.getByTestId("pinned-message");
+    this.pinnedMessageButtonGoTo = this.page.getByTestId(
+      "pinned-message-button-go-to",
+    );
+    this.pinnedMessageButtonUnpin = this.page.getByTestId(
+      "pinned-message-button-unpin",
+    );
+    this.pinnedMessageProfilePicture = this.page.getByTestId(
+      "pinned-message-profile-picture",
+    );
+    this.pinnedMessageProfileStatusIndicator =
+      this.pinnedMessageProfilePicture.getByTestId("status-indicator");
+    this.pinnedMessageSender = this.page.getByTestId("pinned-message-sender");
+    this.pinnedMessageText = this.page.getByTestId("pinned-message-text");
+    this.pinnedMessageTimestamp = this.page.getByTestId(
+      "pinned-message-timestamp",
+    );
+    this.pinnedMessagesContainer = this.page.getByTestId(
+      "pinned-messages-container",
+    );
+    this.pinnedMessagesEmpty = this.page.getByTestId("pinned-messages-empty");
+    this.sectionAddSomeone = this.page.getByTestId("section-add-someone");
+    this.topbar = this.page.getByTestId("topbar");
+  }
+}
