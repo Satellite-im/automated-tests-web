ac0dc36af0fa62888afa18378291fad991dd98aa
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-10-11 13:02:04 -0600

    update(pom): pom for incoming call

--- playwright/PageObjects/CallElements/IncomingCall.ts
+++ playwright/PageObjects/CallElements/IncomingCall.ts
@@ -1,18 +1,67 @@
 import MainPage from "../MainPage";
-import { type Locator, type Page } from "@playwright/test";
+import { type Locator, type Page, expect } from "@playwright/test";
 
 export class IncomingCall extends MainPage {
+  readonly buttonAcceptIncomingCall: Locator;
+  readonly buttonDenyIncomingCall: Locator;
   readonly incomingCallModal: Locator;
-  readonly incomingProfilePicture: Locator;
+  readonly incomingCallProfilePicture: Locator;
+  readonly incomingCallProfilePictureIdenticon: Locator;
+  readonly incomingCallProfilePictureImage: Locator;
+  readonly incomingCallStatus: Locator;
+  readonly incomingCallStatusIndicator: Locator;
+  readonly incomingCallUsername: Locator;
 
   constructor(
     public readonly page: Page,
     public readonly viewport: string,
   ) {
     super(page, viewport);
+    this.buttonAcceptIncomingCall = this.incomingCallModal.getByTestId(
+      "button-accept-incoming-call",
+    );
+    this.buttonDenyIncomingCall = this.incomingCallModal.getByTestId(
+      "button-deny-incoming-call",
+    );
     this.incomingCallModal = this.page.getByTestId("incoming-call");
-    this.incomingProfilePicture = this.incomingCallModal.getByTestId(
+    this.incomingCallProfilePicture = this.incomingCallModal.getByTestId(
       "friend-profile-picture",
+    );
+    this.incomingCallProfilePictureIdenticon =
+      this.incomingCallProfilePicture.locator(".identicon img");
+    this.incomingCallProfilePictureImage =
+      this.incomingCallProfilePicture.locator("img");
+    this.incomingCallStatus = this.incomingCallModal.getByTestId(
+      "incoming-call-status",
+    );
+    this.incomingCallStatusIndicator =
+      this.incomingCallModal.getByTestId("status-indicator");
+    this.incomingCallUsername = this.incomingCallModal.getByTestId(
+      "incoming-call-username",
+    );
+  }
+
+  async acceptIncomingCall() {
+    await this.buttonAcceptIncomingCall.click();
+  }
+
+  async denyIncomingCall() {
+    await this.buttonDenyIncomingCall.click();
+  }
+
+  async validateIncomingCallModal(
+    username: string,
+    status: string,
+    imageSrc: string,
+  ) {
+    await expect(this.incomingCallUsername).toHaveText(username);
+    await expect(this.incomingCallStatus).toHaveText(status);
+    await expect(this.incomingCallStatusIndicator).toHaveClass(
+      /.*\bonline\b.*/,
+    );
+    await expect(this.incomingCallProfilePictureImage).toHaveAttribute(
+      "src",
+      imageSrc,
     );
   }
 }
