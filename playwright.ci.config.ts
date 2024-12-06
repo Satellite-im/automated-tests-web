cc6399fcb8a1fd0f596fff9ab9c51df56dead574
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-12-06 13:05:06 -0600

    update(config): increase action timeout times

--- playwright.ci.config.ts
+++ playwright.ci.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     testIdAttribute: "data-cy",
-    actionTimeout: 5000,
+    actionTimeout: 10000,
     video: "retain-on-failure",
     screenshot: "only-on-failure",
     trace: "on-first-retry",
