313faedd89cd5e88bf432979aeba617f86165360
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-12-12 13:22:00 -0600

    update(ci): start webserver independently

--- playwright.ci.config.ts
+++ playwright.ci.config.ts
@@ -95,9 +95,9 @@ export default defineConfig({
   ],
 
   // Run your local dev server before starting the tests
-  webServer: {
-    command: "cd .. && npm run dev",
-    url: "http://127.0.0.1:5173",
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: "cd .. && npm run dev",
+  //   url: "http://127.0.0.1:5173",
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
