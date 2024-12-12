6e8bd231fd6026140c14115961a8ce3e68269d7d
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-12-12 15:46:24 -0600

    update(ci): base url

--- playwright.ci.config.ts
+++ playwright.ci.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
   timeout: 60000,
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:5173/",
+    baseURL: "http://127.0.0.1:5173",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     testIdAttribute: "data-cy",
