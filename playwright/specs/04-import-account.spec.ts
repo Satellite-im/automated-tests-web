7146fbeb30ea3817e1433ca8c315791eed9bb58e
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-12-19 11:10:32 -0600

    update(test): last changes

--- playwright/specs/04-import-account.spec.ts
+++ playwright/specs/04-import-account.spec.ts
@@ -1,10 +1,10 @@
 import { CreateOrImportPage } from "playwright/PageObjects/CreateOrImport";
-import { test } from "../fixtures/setup";
+import { expect, test } from "../fixtures/setup";
 import { ImportAccountPage } from "playwright/PageObjects/ImportAccount";
 import { LoginPinPage } from "playwright/PageObjects/LoginPin";
 
 test.describe("Import Account Tests", () => {
-  test("U1 - Import Account - Valid PIN and go back", async ({
+  test("U1 - Import Account - Negative Input tests, go back and scramble keypad", async ({
     enterPinUserContext,
   }) => {
     const page = enterPinUserContext.page;
@@ -26,6 +26,49 @@ test.describe("Import Account Tests", () => {
       await importAccount.validatePageIsLoaded();
       await importAccount.clickOnGoBack();
       await createOrImport.validatePageIsDisplayed();
+    });
+
+    await test.step("Go to Import Account and Validate PIN settings options", async () => {
+      await createOrImport.clickImportAccount();
+      await loginPinPage.goToPinSettings();
+      await expect(loginPinPage.scrambleKeypadLabel).toBeVisible();
+      await expect(loginPinPage.scrambleKeypadLabel).toHaveText(
+        "Scramble keypad?",
+      );
+      await expect(loginPinPage.stayUnlockedLabel).toBeVisible();
+      await expect(loginPinPage.stayUnlockedLabel).toHaveText("Stay unlocked?");
+    });
+
+    await test.step("Validate scramble pad change the order of pin input buttons", async () => {
+      await loginPinPage.clickScrambleKeypadSwitch();
+      const newKeyOrder =
+        await loginPinPage.pinKeypad.getAttribute("data-keyorder");
+      expect(newKeyOrder).not.toEqual("1,2,3,4,5,6,7,8,9,0");
+    });
+
+    await test.step("Scramble keypad is disabled again by the user and order of keys is restored", async () => {
+      await loginPinPage.clickScrambleKeypadSwitch();
+      await expect(loginPinPage.pinKeypad).toHaveAttribute(
+        "data-keyorder",
+        "1,2,3,4,5,6,7,8,9,0",
+      );
+    });
+
+    await test.step("PIN should have at least 3 digits otherwise continue button is disabled", async () => {
+      await loginPinPage.enterPin("123");
+      await loginPinPage.validateConfirmButtonIsDisabled();
+    });
+
+    await test.step("Validate user cannot Enter a PIN with more than 8 digits", async () => {
+      await loginPinPage.enterPin("12345678901234");
+      const count = await loginPinPage.pinDotFilled.count();
+      expect(count).toEqual(8);
+    });
+
+    await test.step("Red button should reset pin input", async () => {
+      await loginPinPage.buttonClearInput.click();
+      const count = await loginPinPage.pinDotFilled.count();
+      expect(count).toEqual(0);
     });
   });
 
